1a32fce6aaf0ad5dc5f089e4774bf8c81c024071
RickR <rrein@akamai.com>
2015-07-07 00:30:26 -0400

    dont require minitest

--- lib/foghorn/minitest.rb
+++ lib/foghorn/minitest.rb
@@ -1,15 +1,6 @@
-require 'minitest/assertions'
-
 module Foghorn::Minitest
-  module Minitest::Assertions
-    def i_do_declare(act, operator, exp)
-      if operator.to_sym == :to_be
-        assert_equal(exp, act) if operator == :to_be
-      elsif operator.to_sym == :to_not_be
-        refute_equal(exp, act) if operator == :to_not_be
-      else
-        fail 'Operator not in :to_be or :to_not_be'
-      end
-    end
+  def self.included(base)
+    require 'minitest/assertions'
+    require_relative 'minitest/assertions'
   end
 end
